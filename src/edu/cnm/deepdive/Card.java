9df9aa6a78a3915b40a7f9754d6d425c8230d8fe
ryanlazo <ryanlazo12@gmail.com>
2018-02-07 17:08:48 -0500

    added a merge sort feature

--- src/edu/cnm/deepdive/Card.java
+++ src/edu/cnm/deepdive/Card.java
@@ -1,6 +1,6 @@
 package edu.cnm.deepdive;
 
-public class Card {
+public class Card implements Comparable<Card>{
 
   private Rank rank;
   private Suit suit;
@@ -21,5 +21,17 @@ public class Card {
   @Override
   public String toString() {
     return String.format("%2s%s", rank, suit);
+  }
+
+
+  @Override
+  public int compareTo(Card otherCard) {
+  int suitCompare = suit.compareTo(otherCard.suit);
+  if (suitCompare == 0) {
+    return rank.compareTo(otherCard.rank);
+    }else {
+    return suitCompare;
+  }
+
   }
 }
--- src/edu/cnm/deepdive/DeckTest.java
+++ src/edu/cnm/deepdive/DeckTest.java
@@ -5,9 +5,13 @@ public class DeckTest {
   public static void main (String[] args) {
     Deck deck = new Deck();
     deck.shuffle();
-    for(Card card: deck.getCards()) {
+    for (Card card : deck.getCards()) {
+      System.out.println(card);
+    }
+    System.out.println("---Sorted---");
+    deck.sort();
+    for (Card card : deck.getCards()) {
       System.out.println(card);
     }
   }
-
 }
