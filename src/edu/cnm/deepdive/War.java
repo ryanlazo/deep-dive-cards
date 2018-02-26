f5ea4aaf3ed3c2b54e08decf05cddc3c2d5f72b9
ryanlazo <ryanlazo12@gmail.com>
2018-02-26 10:11:09 -0500

    Blackjack homework

--- src/edu/cnm/deepdive/War.java
+++ src/edu/cnm/deepdive/War.java
@@ -6,6 +6,11 @@ import java.util.List;
 
 public class War {
 
+  private static final String DRAW_FORMAT = "Player1: %s\tPlayer2: %s\t";
+  private static final String STANDALONE_DRAW_FORMAT = "DRAW_FORMAT +%n";
+  private static final String WIN_FORMAT = "Player %d wins %d cards%n";
+  private static final String WAR_ANNOUNCEMENT = "I hope you know\u2026this means war!";
+  private static final String GAME_OVER_FORMAT = "Game Over. Player1: %d, Player 2: %d%n";
   private boolean aceHigh;
   private boolean suicide;
   private List<Card> hand1;
@@ -14,7 +19,22 @@ public class War {
   private int tally2;
   private Comparator<Card> comp;
 
-  {
+
+
+  public War(boolean aceHigh, boolean suicide) {
+    this.aceHigh = aceHigh;
+    this.suicide = suicide;
+    reset();
+  }
+
+  public War(boolean aceHigh) {
+    this(aceHigh, false);
+  }
+
+  public War() {
+    this(true);
+  }
+  public void reset() {
     comp = new WarComparator();
     hand1 = new LinkedList<>();
     hand2 = new LinkedList<>();
@@ -30,35 +50,21 @@ public class War {
     tally1 = 0;
     tally2 = 0;
   }
-
-  public War(boolean aceHigh, boolean suicide) {
-    this.aceHigh = aceHigh;
-    this.suicide = suicide;
-  }
-
-  public War(boolean aceHigh) {
-    this(aceHigh, false);
-  }
-
-  public War() {
-    this(true);
-  }
-
   public void play() {
     while (!hand1.isEmpty()) {
       Card card1 = hand1.remove(0);
       Card card2 = hand2.remove(0);
       int comparison = comp.compare(card1, card2);
-      System.out.printf("Player1: %s\tPlayer2: %s\t", card1, card2);
+      System.out.printf(DRAW_FORMAT, card1, card2);
       if (comp.compare(card1, card2) > 0) {
         tally1 += 2;
-        System.out.println("Player 1 wins 2 cards");
+        System.out.printf(WIN_FORMAT, 1, 2);
       } else if (comparison < 0) {
         tally2 += 2;
-        System.out.println("Player 2 wins 2 cards");
+        System.out.printf(WIN_FORMAT, 2, 2);
       }else {
         //WAR!!!
-        System.out.println("I hope you know...this means war!");
+        System.out.println(WAR_ANNOUNCEMENT);
         int pile = 2;
         while(comparison == 0 && !hand1.isEmpty()) {
           int turn = Math.min(4, hand1.size());
@@ -66,21 +72,21 @@ public class War {
             card1 = hand1.remove(0);
             card2 = hand2.remove(0);
             pile += 2;
-            System.out.printf("Player1: %s\tPlayer2: %s\t%n", card1, card2);
+            System.out.printf(STANDALONE_DRAW_FORMAT, card1, card2);
           }
           comparison = comp.compare(card1, card2);
         }
         if(comparison > 0) {
           tally1 += pile;
-          System.out.printf("Player 1 wins %d cards%n", pile);
+          System.out.printf(WIN_FORMAT, 1, pile);
         } else if (comparison < 0){
           tally2 += pile;
-          System.out.printf("Player 3 wins %d cards%n", pile);
+          System.out.printf(WIN_FORMAT, 2, pile);
         }
 
       }
     }
-    System.out.printf("Game Over. Player1: %d, Player 2: %d%n", tally1, tally2);
+    System.out.printf(GAME_OVER_FORMAT, tally1, tally2);
   }
 
   private class WarComparator implements Comparator<Card> {
---
+++ src/edu/cnm/deepdive/blackjack/Hand.java
@@ -0,0 +1,101 @@
+package edu.cnm.deepdive.blackjack;
+
+import edu.cnm.deepdive.Card;
+import edu.cnm.deepdive.Rank;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class Hand implements Comparable<Hand> {
+
+  private static final int HIGH_ACE_INCREMENT = 10;
+  private static final int WINNING_TOTAL = 21;
+  private static final int BLACKJACK_HAND = 2;
+  private static Map<Rank, Integer> values;
+  private List<Card> cards;
+  private boolean bust;
+  private boolean blackjack;
+  private int value;
+
+  static {
+    values = Map.ofEntries(
+        Map.entry(Rank.ACE, 1),
+        Map.entry(Rank.TWO, 2),
+        Map.entry(Rank.THREE, 3),
+        Map.entry(Rank.FOUR, 4),
+        Map.entry(Rank.FIVE, 5),
+        Map.entry(Rank.SIX, 6),
+        Map.entry(Rank.SEVEN, 7),
+        Map.entry(Rank.EIGHT, 8),
+        Map.entry(Rank.NINE, 9),
+        Map.entry(Rank.TEN, 10),
+        Map.entry(Rank.JACK, 10),
+        Map.entry(Rank.QUEEN, 10),
+        Map.entry(Rank.KING, 10)
+    );
+
+  }
+  public Hand() {
+    cards = new LinkedList<>();
+  }
+
+  public void add(Card card) {
+    cards.add(card);
+    update();
+  }
+
+  public void clear() {
+    cards.clear();
+    bust = false;
+    blackjack = false;
+    value = 0;
+  }
+
+  public boolean isBust() {
+    return bust;
+  }
+
+  public boolean isBlackjack() {
+    return blackjack;
+  }
+
+  public int value() {
+    return value;
+  }
+
+  private void update() {
+    int aces = 0;
+    value = 0;
+    bust = false;
+    blackjack = false;
+
+    for (Card card : cards) {
+      value += values.get(card.getRank());
+      if (card.getRank() == Rank.ACE) {
+        aces++;
+      }
+
+    }
+    for(int i = 0; i < aces; i++) {
+      if (value + HIGH_ACE_INCREMENT <= WINNING_TOTAL) {
+        value += HIGH_ACE_INCREMENT;
+      }
+    }
+    if (value > WINNING_TOTAL) {
+      bust = true;
+      value = 0;
+
+    }else if (value == WINNING_TOTAL && cards.size() == BLACKJACK_HAND) {
+      blackjack = true;
+    }
+  }
+
+@Override
+  public int compareTo(Hand hands) {
+   int result = Boolean.compare(isBlackjack(), hands.isBlackjack());
+   if (result == 0) {
+     result = Integer.compare(this.value(), hands.value);
+   }
+    return result;
+  }
+}
